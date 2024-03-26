9a189f1bd3a64e2400bb5afdaf0719bfd0b71b42
watsuyo <watsuyo.dev@gmail.com>
2024-03-26 13:58:59 +0100

    Refactor NotionPageClient component

--- app/_components/NotionPageClient.tsx
+++ app/_components/NotionPageClient.tsx
@@ -1,36 +1,32 @@
 import Head from "next/head";
+import { NotionRenderer } from "react-notion-components";
+import { getNotionMetaData } from "../_libs/getNotionData";
 
-import "prismjs/themes/prism-tomorrow.css";
-// import { NotionRenderer } from "@watusyo/react-notion";
-// import "react-notion/src/styles.css";
-import { NotionRenderer } from "../../../react-notion-sushi";
-import "../../../react-notion-sushi/src/styles.css";
+const NotionPageClient = async ({ id }: { id: string }) => {
+	const fetchPage = async () => {
+		const response = await fetch(
+			`https://watsuyo-notion-api-worker.classmethodeurope.workers.dev/v1/page/${id}`,
+		);
+		return await response.json();
+	};
 
-const NotionPageClient = async ({ id }: {
-  id: string;
-}) => {
-  const fetchPage = async () => {
-    const response = await fetch(
-      `https://watsuyo-notion-api-worker.classmethodeurope.workers.dev/v1/page/${id}`
-    );
-    return await response.json();
-  };
+	const blocks = await fetchPage();
 
-  const blocks = await fetchPage();
+	const metaData = await getNotionMetaData(id);
 
-  return (
-    <div>
-      <Head>
-        <style>{`body { background-color: #fff;}`}</style>
-        <title>react-notion example</title>
-      </Head>
-      <NotionRenderer
-        blockMap={blocks}
-        fullPage
-        hideHeader
-      />
-    </div>
-  );
+	return (
+		<div>
+			<Head>
+				<title>react-notion example</title>
+			</Head>
+			<NotionRenderer
+				blockMap={blocks}
+				fullPage
+				hideHeader
+				metaData={metaData}
+			/>
+		</div>
+	);
 };
 
 export default NotionPageClient;
